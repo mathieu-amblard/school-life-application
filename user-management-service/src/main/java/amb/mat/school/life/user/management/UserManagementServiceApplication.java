837bcd2bb948cf7612671f345a4a8a2c2106d19b
Mathieu AMBLARD (u118971) <mathieu.amblard@mobi.ch>
2024-03-14 21:35:11 +0100

    feat: init user management service

---
+++ user-management-service/src/main/java/amb/mat/school/life/user/management/UserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package amb.mat.school.life.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementServiceApplication.class, args);
+	}
+
+}
